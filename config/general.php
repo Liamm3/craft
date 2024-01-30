e673b8e5be916a3ca4286c5e2d3e6b7331fe5e51
Liamm3 <liam.wohlfart@tuta.io>
2024-01-30 13:25:55 +0100

    add new general.php and remove matrix blocks

--- config/general.php
+++ config/general.php
@@ -18,29 +18,29 @@ return [
         'maxRevisions'                       => 20,
         'defaultImageQuality'                => 82,
         'maxUploadFileSize'                  => 134217728, // 128MB
-        'userSessionDuration'                => 7200,
         'phpMaxMemoryLimit'                  => '1024M',
+        'enableCsrfProtection'               => true,
         'addTrailingSlashesToUrls'           => true,
         'generateTransformsBeforePageLoad'   => true,
         'limitAutoSlugsToAscii'              => true,
         'optimizeImageFilesize'              => true,
-        'convertFilenamesToAscii'            => true,
         'rotateImagesOnUploadByExifData'     => true,
         'omitScriptNameInUrls'               => true,
+        'convertFilenamesToAscii'            => true,
         'backupOnUpdate'                     => true,
-        'autosaveDrafts'                     => false,
-        'requireMatchingUserAgentForSession' => false,
         'preserveExifData'                   => false,
         'preserveImageColorProfiles'         => false,
         'allowUppercaseInSlug'               => false,
         'sendPoweredByHeader'                => false,
         'transformGifs'                      => false,
+        'requireMatchingUserAgentForSession' => false,
         'enableGql'                          => false,
+        'autosaveDrafts'                     => false,
         'csrfTokenName'                      => 'CRAFT_CSRF_TOKEN',
         'pageTrigger'                        => 'page',
-        'defaultCpLanguage'                  => 'de',
+        'errorTemplatePrefix'                => 'errors/',
         'timezone'                           => 'Europe/Berlin',
-        'errorTemplatePrefix'                => 'system/errors/',
+        // 'cpTrigger'                          => App::env('CP_TRIGGER') ?: 'admin',
         'securityKey'                        => App::env('SECURITY_KEY'),
         'isSystemLive'                       => App::env('IS_SYSTEM_LIVE'),
         'devMode'                            => App::env('DEV_MODE'),
@@ -48,28 +48,24 @@ return [
         'runQueueAutomatically'              => App::env('RUN_QUEUE_AUTOMATICALLY'),
         'enableTemplateCaching'              => App::env('ENABLE_TEMPLATE_CACHING'),
         'disallowRobots'                     => App::env('DISALLOW_ROBOTS'),
-        // 'cpTrigger'                          => App::env('CP_TRIGGER'),
-        'testToEmailAddress'                 => App::env('TEST_TO_EMAIL') ?: null,
 
         'defaultSearchTermOptions' => [
             'subLeft'  => true,
-            'subRight' => true,
+            'subRight' => true
         ],
 
         'aliases' => [
-            // General
-            'siteUrl'            => $_SERVER['SERVER_NAME'],
-
-            // DE
-            'baseUrl'            => App::env('DEFAULT_SITE_URL'),
-            'basePath'           => $_SERVER['DOCUMENT_ROOT'],
-
-            // DE Aliases for console
-            '@web'               => App::env('DEFAULT_SITE_URL'),
-            '@webroot'           => $_SERVER['DOCUMENT_ROOT'],
-
-            '@asset_url'         => App::env('ASSET_BASE_URL'),
-            '@asset_path'        => CRAFT_BASE_PATH . App::env('ASSET_BASE_PATH'),
+            'baseUrl'  => getenv('PRIMARY_SITE_URL'),
+            'basePath' => $_SERVER['DOCUMENT_ROOT'],
+            '@web'     => getenv('PRIMARY_SITE_URL')
         ]
+    ],
+
+    'dev' => [
+        'testToEmailAddress' => 'frank@craft-unit.de'
+    ],
+
+    'staging' => [
+        'testToEmailAddress' => 'frank@craft-unit.de'
     ]
 ];
